80a2afbdc832cca43cd9d2aa5111eb4ade4f3cfb
hadihimself <alhadix170@gmail.com>
2022-12-19 03:36:05 -0800

    print99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,9 +15,9 @@ int main(void)
 {
 	int x , y ;
 
-	for (x = 0 , x <= 98 , x++)
+	for (x = 0 ; x <= 98 ; x++)
 	{
-		for (y = x + 1 , y <= 99 , y++)
+		for (y = x + 1 ; y <= 99 ; y++)
 		{
 			putchar((x / 10) + '0');
 			putchar((x % 10) + '0');
@@ -25,7 +25,7 @@ int main(void)
 			putchar((y / 10) + '0');
 			putchar((y % 10) + '0');
 
-			if (x == 98 && y = 99)
+			if (x == 98 && y == 99)
 				continue;
 
 			putchar(',');
