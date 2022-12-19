8fc5ff6f428991110f881b3d89042a0e32e5821d
hadihimself <alhadix170@gmail.com>
2022-12-19 05:56:01 -0800

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main:in main functionj
+ * main - in main functionj
  * take a break
+ * Return: always 0
  */
 
 int main(void)
 {
 	char *h = "_putchar";
-
 	int i;
+
 	for (i = 0 ; i <= 7 ; i++)
 	{
 		_putchar(*h);
