c31f539a98ed529eef77b35ca3234c64b3507c9b
hadihimself <alhadix170@gmail.com>
2023-01-03 17:22:58 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,10 @@
 
 /**
  *char *_memset - set memory with byte b
- *returns 
+ *@s: pointed destination
+ *@b: constant byte
+ *@n: bytes
+ * Return : always 0 
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -10,8 +13,6 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0 ; i < n ; i ++)
-	{
 		s[i] = b;
-	}
 	return s;
 }
