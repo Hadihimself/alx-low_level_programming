6a1f2c71268629680f8c297618fc762174fba1a6
Hadihimself <Alhadix170@gmail.com>
2024-07-20 01:53:44 +0000

    optional

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,8 +7,21 @@
  * Return: Always 0.
  */
 
-int main(void)
+void main(void)
 {
-	printf("612852475143\n");
-	return (0);
+    double n = 612852475143;
+    while (n % 2 == 0) {
+        printf("%d \n", 2);
+        n = n / 2;
+    }
+
+    for (int i = 3; i * i <= n; i = i + 2) {
+        while (n % i == 0) {
+            printf("%d \n ", i);
+            n = n / i;
+        }
+    }
+
+    if (n > 2)
+        printf("%d \n", n);
 }
