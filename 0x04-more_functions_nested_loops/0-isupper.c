c39be78a0636855c0d72b7b01851816bbbe4dfc4
Hadihimself <Alhadix170@gmail.com>
2024-07-20 01:10:21 +0000

    Isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * This function returns the cahacter and
- * 1 if it is upper case 
- * 0 if it is lower case
+ * _isupper - check the code for Holberton School students.
+ * @c: Character input
+ * Return: Always 0.
  */
 
-int _isupper(char c)
+int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
-	{
-	 return(1);
-	}
-	else 
-		return(0);	
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
