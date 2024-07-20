534952133838053c46704aab5e2e59915e368e80
Hadihimself <Alhadix170@gmail.com>
2024-07-20 01:24:30 +0000

    three codes

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code for Alhadi School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num == '2' || num == '4')
+			continue;
+		else
+			_putchar(num);
+	}
+	_putchar('\n');
+}
